fe49e4503b170215a27104510778e7fcd00ee121
James <james@james-VirtualBox.(none)>
2013-07-04 18:56:37 +0100

    Updated to read file

--- web.js
+++ web.js
@@ -3,7 +3,9 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+    var data = fs.readFileSync("index.html");
+    var output = data.toString();
+  response.send(output);
 });
 
 var port = process.env.PORT || 5000;
